11b23c591feb3eb4e253d073ff56682260477b28
Alin Ciu <alin.ciu@endava.com>
2020-07-11 01:37:56 +0300

    -

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,13 +1,120 @@
-import { FunctionalComponent, h } from "preact";
+import { h, Component } from "preact";
 import * as style from "./style.css";
+import { LoginRequest, User } from "../../grpc/auth_pb";
+import { AuthClient } from "../../grpc/AuthServiceClientPb";
+import { GetWeatherRequest } from '../../grpc/weather_pb';
+import { WeatherClient } from '../../grpc/WeatherServiceClientPb';
 
-const Home: FunctionalComponent = () => {
-    return (
-        <div class={style.home}>
-            <h1>Home</h1>
-            <p>This is the Home component.</p>
-        </div>
-    );
-};
+class Home extends Component {
+    state = { user: "", pass: "", loggedInUser: null, error: "", weather: "" };
+    authClient = new AuthClient("/services/auth");
+    weatherClient = new WeatherClient("/services/weather");
+
+    logout = () => {
+        this.setState({ ...this.state, error: "", loggedInUser: null, weather: "" });
+    };
+
+    getWeather = () => {
+        const getWeatherRequest = new GetWeatherRequest();
+        getWeatherRequest.setUsemetric(true);
+        getWeatherRequest.setAddress("Something");
+        this.weatherClient.getWeather(getWeatherRequest, null).then(r => {
+            this.setState({...this.state, weather: JSON.stringify(r.toObject())});
+        }).catch(e => {
+            this.setState({...this.state, weather: JSON.stringify(e)});
+        });
+    };
+
+    onSubmit = (e: any) => {
+        e.preventDefault();
+        const loginRequest = new LoginRequest();
+        loginRequest.setUsername(this.state.user);
+        loginRequest.setPassword(this.state.pass);
+        this.authClient
+            .login(loginRequest, null)
+            .then(r => {
+                if (r.getSuccesful()) {
+                    this.setState({
+                        ...this.state,
+                        loggedInUser: r.getUser(),
+                        error: ""
+                    });
+                    return;
+                }
+                this.setState({
+                    ...this.state,
+                    loggedInUser: null,
+                    weather: "",
+                    error: r.getErrorsList().join(";")
+                });
+            })
+            .catch(e => {
+                this.setState({
+                    ...this.state,
+                    loggedInUser: null,
+                    weather: "",
+                    error: JSON.stringify(e)
+                });
+            });
+    };
+
+    onUserInput = (e: any) => {
+        const { value } = e.target;
+        this.setState({ ...this.state, user: value });
+    };
+
+    onPassInput = (e: any) => {
+        const { value } = e.target;
+        this.setState({ ...this.state, pass: value });
+    };
+
+    render(_, { user, pass, loggedInUser, error, weather }) {
+        return (
+            <div class={style.home}>
+                <h1>gRPC PoC</h1>
+                <p style="color: red">{error}</p>
+                <p>
+                    {loggedInUser == null
+                        ? "Not logged in"
+                        : "Logged in as " +
+                          (loggedInUser as User).getUsername()}
+                </p>
+                {loggedInUser == null ? (
+                    <form onSubmit={this.onSubmit}>
+                        <label for="user">User</label>
+                        <input
+                            name="user"
+                            id="user"
+                            type="text"
+                            value={user}
+                            onInput={this.onUserInput}
+                        />
+                        <br />
+                        <label for="pass">Pass</label>
+                        <input
+                            name="pass"
+                            id="pass"
+                            type="password"
+                            value={pass}
+                            onInput={this.onPassInput}
+                        />
+                        <br />
+                        <button type="submit">Login</button>
+                    </form>
+                ) : (
+                    <button onClick={this.logout}>Logout</button>
+                )}
+                <br />
+                {loggedInUser != null ? (
+                    <button onClick={this.getWeather}>Get Weather</button>
+                ) : (
+                    <div></div>
+                )}
+                <br />
+                <p>{weather}</p>
+            </div>
+        );
+    }
+}
 
 export default Home;
